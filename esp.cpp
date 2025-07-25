de7e91992aee87de4e3b33a43d159e53b3795410
Mohammed Zoabi <zhamodi@gmail.com>
2025-07-25 13:10:14 +0300

    הגדרטות חומרה

--- esp.cpp
+++ esp.cpp
@@ -2,9 +2,15 @@
 #include <WebSocketClient.h>
 #include <ArduinoJson.h>
 #include <Ticker.h>
+// WiFi settings לשנות בהתאם
 const char* ssid = "YOUR_WIFI_SSID";
 const char* password = "YOUR_WIFI_PASSWORD";
+//websocket
 const char* ws_host = "YOUR_WEBSOCKET_SERVER";
 const int ws_port = 8080;
 const char* ws_path = "/ws";
 const char* device_id = "DISTRESS_001";
+// Pins
+const int buttonPin = 4;  
+const int ledPin = 5;     
+const int buzzerPin = 18
