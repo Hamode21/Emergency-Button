69868b9b5a874c29ac6421103e3a200b2b33c0e0
Mohammed Zoabi <zhamodi@gmail.com>
2025-07-25 12:52:04 +0300

    תקשורת
    חיבור ל WIFI

--- esp.cpp
+++ esp.cpp
@@ -6,3 +6,10 @@ const int ledPin = 5;
 const int buzzerPin = 18; 
 const char* ssid = "SSID_שלך";
 const char* mqtt_server = "broker.hivemq.com";
+void setup_wifi() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+}
