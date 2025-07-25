f8dfc3e52fc28d4d24d8a4ddfa00ec33a5508702
Mohammed Zoabi <zhamodi@gmail.com>
2025-07-25 13:08:06 +0300

    websocket settings

--- esp.cpp
+++ esp.cpp
@@ -4,3 +4,7 @@
 #include <Ticker.h>
 const char* ssid = "YOUR_WIFI_SSID";
 const char* password = "YOUR_WIFI_PASSWORD";
+const char* ws_host = "YOUR_WEBSOCKET_SERVER";
+const int ws_port = 8080;
+const char* ws_path = "/ws";
+const char* device_id = "DISTRESS_001";
